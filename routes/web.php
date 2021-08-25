04cf313569d338ec0a2d77bbdae2b6d3388ab933
Reiko <reiko.yokoyama@rakuten-sec.co.jp>
2021-08-26 08:54:26 +0900

    第３章　第５部

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+// http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定
+Route :: get('xxx', 'xxx\AAAController@bbb');
+
+// getメソッドでadmin/profile/create にアクセスしたら ProfileController の add Action に、
+// admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
+
+Route :: group(['prefix' => 'admin'], function(){
+    Route :: get('profile/create', 'Admin\Profilecontroller@add');
+    Route :: get('profile/edit', 'Admin\Profilecontroller@edit');
+
+});
